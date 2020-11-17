3d0e92929ef5215f0bc2e066216de9d582f4efc3
calvin-brown <44214170+calvin-brown@users.noreply.github.com>
2020-11-17 12:24:19 -0800

    Tweaked stereo parameters

--- extract_raw.py
+++ extract_raw.py
@@ -13,7 +13,7 @@ from matplotlib import pyplot as plt
 r = 3040
 c = 4056
 
-with open('image.jpg', 'rb') as f:
+with open('image_mid.jpg', 'rb') as f:
     data = f.read()
 data = list(data)
 
@@ -36,13 +36,14 @@ bayer[:, 1::2] = (img[:, 1::3] << 4) + (img[:, 2::3] >> 4)
 # plt.figure()
 # plt.imshow(np.stack((red_img, green_img, blue_img), axis=-1) / 2000)
 
-# green1 = bayer[::2, 1::2]
-# green2 = bayer[1::2, ::2] # green2 is shifted "southwest" of green1
-green1 = bayer[::4, 1::4]
-green2 = bayer[::4, 3::4]
+green1 = bayer[::2, 1::2]
+green2 = bayer[1::2, ::2] # green2 is shifted "southwest" of green1
+# green1 = bayer[::4, 1::4]
+# green2 = bayer[::4, 3::4]
 
 plt.figure()
-plt.imshow(green1 - green2)
+plt.imshow(green1.astype('double') - green2.astype('double'))
+plt.colorbar()
 
 # convert images to 8-bit for openCV
 green1_8bit = np.zeros(green1.shape, dtype='uint8')
@@ -52,45 +53,51 @@ green2_8bit = np.zeros(green2.shape, dtype='uint8')
 green2_8bit = cv.normalize(green2, green2_8bit, alpha=0, beta=255, norm_type=cv.NORM_MINMAX)
 green2_8bit = green2_8bit.astype('uint8')
 
-blocks = [11, 21, 31, 41]
+stereo = cv.StereoBM_create(numDisparities=16, blockSize=41)
+disparity = stereo.compute(green1_8bit, green2_8bit)
+plt.figure()
+plt.imshow(disparity)
+plt.colorbar()
 
-for i in range(len(blocks)):
+# blocks = [11, 21, 31, 41]
+
+# for i in range(len(blocks)):
     
-    # compute depth (disparity) image
-    window_size=5
-    # stereo = cv.StereoSGBM_create(minDisparity=0,
-    #                               numDisparities=16,
-    #                               blockSize=16,
-    #                               P1=8*3*window_size**2,
-    #                               P2=32*3*window_size**2,
-    #                               disp12MaxDiff=1,
-    #                               uniquenessRatio=10,
-    #                               speckleWindowSize=100,
-    #                               speckleRange=32
-    #                               )
-    # stereo = cv.StereoSGBM_create(minDisparity=0,
-    #                               numDisparities=16,
-    #                               blockSize=16,
-    #                               P1=8*3*window_size**2,
-    #                               P2=32*3*window_size**2,
-    #                               disp12MaxDiff=1,
-    #                               uniquenessRatio=10,
-    #                               speckleWindowSize=100,
-    #                               speckleRange=32,
-    #                               textureThreshold=10)
-    stereo = cv.StereoSGBM_create(minDisparity=-1,
-                                  numDisparities=64,
-                                  blockSize=5,
-                                  uniquenessRatio=5,
-                                  speckleWindowSize=5,
-                                  speckleRange=5,
-                                  disp12MaxDiff=1,
-                                  P1=8*3*window_size**2,
-                                  P2=32*3*window_size**2)
-    # stereo = cv.StereoBM_create(numDisparities=16, blockSize=blocks[i])
-    disparity = stereo.compute(green1_8bit, green2_8bit)
+#     # compute depth (disparity) image
+#     window_size=5
+#     # stereo = cv.StereoSGBM_create(minDisparity=0,
+#     #                               numDisparities=16,
+#     #                               blockSize=16,
+#     #                               P1=8*3*window_size**2,
+#     #                               P2=32*3*window_size**2,
+#     #                               disp12MaxDiff=1,
+#     #                               uniquenessRatio=10,
+#     #                               speckleWindowSize=100,
+#     #                               speckleRange=32
+#     #                               )
+#     # stereo = cv.StereoSGBM_create(minDisparity=0,
+#     #                               numDisparities=16,
+#     #                               blockSize=16,
+#     #                               P1=8*3*window_size**2,
+#     #                               P2=32*3*window_size**2,
+#     #                               disp12MaxDiff=1,
+#     #                               uniquenessRatio=10,
+#     #                               speckleWindowSize=100,
+#     #                               speckleRange=32,
+#     #                               textureThreshold=10)
+#     stereo = cv.StereoSGBM_create(minDisparity=-1,
+#                                   numDisparities=64,
+#                                   blockSize=5,
+#                                   uniquenessRatio=5,
+#                                   speckleWindowSize=5,
+#                                   speckleRange=5,
+#                                   disp12MaxDiff=1,
+#                                   P1=8*3*window_size**2,
+#                                   P2=32*3*window_size**2)
+#     # stereo = cv.StereoBM_create(numDisparities=16, blockSize=blocks[i])
+#     disparity = stereo.compute(green1_8bit, green2_8bit)
     
-    plt.figure()
-    # plt.imshow(disparity, 'gray')
-    plt.imshow(disparity)
-    plt.colorbar()
+#     plt.figure()
+#     # plt.imshow(disparity, 'gray')
+#     plt.imshow(disparity)
+#     plt.colorbar()
